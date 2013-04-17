93ec4ec293a0f27ad7760218fd4857abd6395821
r.lane <r.lane@fluidappsdev.com>
2013-04-17 16:25:52 -0700

    User resource routed and root route set to application#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def update
+  end
+  
+  def delete
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MatchMaker::Application.routes.draw do
+  resources :users
+  root :to => "application#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
