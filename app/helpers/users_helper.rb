00610ca3131d8e3ba974587a19946b1fe62e1639
r.lane <r.lane@fluidappsdev.com>
2013-04-18 11:38:07 -0700

    Validation and encryption methods to pass user model tests

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
 module UsersHelper
+  def encrypt_password
+    return unless self.respond_to?(:password) && self.respond_to?(:salt)
+    self.salt = make_salt if new_record?
+    self.password = encrypt_value self.password
+  end
+  
+  private
+    def encrypt_value(string_value)
+      secure_hash "#{self.salt}--#{string_value}"
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{self.password}")
+    end
+    
+    def secure_hash(string_value)
+      Digest::SHA2.hexdigest(string_value)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
 class User < ActiveRecord::Base
+  include UsersHelper
   attr_accessible :email, :password, :salt, :screen_name
+  
+  before_save :encrypt_password  
+  
+  validates :email, :presence => true, 
+        :uniqueness => true, :confirmation => true, 
+        :format => {:with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :screen_name, :presence => true, :uniqueness => true
+  
+  def has_password?(passwordString)
+    self.password == encrypt_value(passwordString)
+  end
+  
+  def authenticate(email, password)
+    user = user.find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(password)
+  end
+  
+  def authenticate_cookie(id, cookie_salt)
+    user = find(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
+
 end
