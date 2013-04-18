a64e1e36d6bfa7b5404f8ee09c6fdef302ad87eb
r.lane <r.lane@fluidappsdev.com>
2013-04-18 11:37:34 -0700

    Initial test coverage for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,25 +3,35 @@ require 'spec_helper'
 describe User do
   
   before(:each) do
-    @user_attribs  = { screen_name: "tester",  email: "test@tester", "Password" => "m434edr02a", 
-        "Password Confirmation" => "m434edr02a" }
+    @user_attribs  = { screen_name: "tester",  email: "test@tester.com", password: "m434edr02a" }
   end
   
   it "should create a new instance with valid attributes" do
     User.create!(@user_attribs);  
   end
   
-  it "should require a screen_name" do
+  it "should reject a blank screen name" do
     no_screen_name_user = User.new @user_attribs.merge(screen_name:'')
     no_screen_name_user.should_not be_valid
   end
   
-  it "should require a valid enail" do
+  it "should reject duplicate screen names" do
+    user1 = User.create!(@user_attribs)
+    user2 = User.new(@user_attribs)
+    user2.should_not be_valid
+  end
+  
+  it "should reject a blank email" do
     no_email_user = User.new @user_attribs.merge(email:'')
     no_email_user.should_not be_valid
   end
   
-  it "should reject duplicate emali address" do
+  it "should reject invalid email format" do
+    invalid_user = User.new(@user_attribs.merge(email:'invalid123'))
+    invalid_user.should_not be_valid
+  end
+  
+  it "should reject duplicate email address" do
     user1 = User.create!(@user_attribs)
     user2 = User.new @user_attribs
     user2.should_not be_valid
@@ -33,9 +43,14 @@ describe User do
       @user = User.create!(@user_attribs)
     end
     
-    it "should have an encrypted password" do
+    it "should have a password value" do
       @user.should respond_to(:password)
       @user.password.should_not be_blank
+      @user.salt.should_not be_blank
+    end
+    
+    it "should have an encrypted password" do
+      @user.password.should_not eq(@user_attribs[:password])
     end
     
     it "should have password compare function" do
@@ -43,7 +58,7 @@ describe User do
     end
     
     it "should have a matching encrypted password" do
-      @user.has_password?(@user_attribs["Password"]).should be_true  
+      @user.has_password?(@user_attribs[:password]).should be_true  
     end
   end
     
